6871482bf4b40b42afdafd3b198391edb2b416b3
R1L <32390660+R1L@users.noreply.github.com>
2020-07-20 14:40:01 +0100

    Added project files
    Added
    html
     -index.html
     -beforeYouStart.html
     -jsCheck.html
     -howTo.html
     -modeSelect.html
     -arMarker.html
     -arLocation.html
    css
     -mdb.min.css
     -stylesheet.css
    js
     -button-delay.js
     -jquery-3.5.1.min.js

---
+++ js/button-delay.js
@@ -0,0 +1,77 @@
+function bys()
+{
+  var count = 1;
+  setInterval(function()
+{
+  if (count == 0)
+  {
+    window.location = 'beforeYouStart.html';
+  }
+  count--;
+},1000);
+}
+
+function jsCheck()
+{
+  var count = 1;
+  setInterval(function()
+{
+  if (count == 0)
+  {
+    window.location = 'jsCheck.html';
+  }
+  count--;
+},1000);
+}
+
+function howTo()
+{
+  var count = 1;
+  setInterval(function()
+{
+  if (count == 0)
+  {
+    window.location = 'howTo.html';
+  }
+  count--;
+},1000);
+}
+
+function modeSelect()
+{
+  var count = 1;
+  setInterval(function()
+{
+  if (count == 0)
+  {
+    window.location = 'modeSelect.html';
+  }
+  count--;
+},1000);
+}
+
+function arMarker()
+{
+  var count = 1;
+  setInterval(function()
+{
+  if (count == 0)
+  {
+    window.location = 'arMarker.html';
+  }
+  count--;
+},1000);
+}
+
+function arLocation()
+{
+  var count = 1;
+  setInterval(function()
+{
+  if (count == 0)
+  {
+    window.location = 'arLocation.html';
+  }
+  count--;
+},1000);
+}
